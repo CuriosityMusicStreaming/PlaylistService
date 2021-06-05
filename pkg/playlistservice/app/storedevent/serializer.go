3c8fe4f9a060cf2c1d377939c8788e3ed81f6418
UsingCoding <extendedmoment@outlook.com>
2021-06-06 01:04:00 +0300

    Добавил сериализацию пейлоада ивентов сервиса плейлистов

--- pkg/playlistservice/app/storedevent/serializer.go
+++ pkg/playlistservice/app/storedevent/serializer.go
@@ -51,6 +51,40 @@ func serializeEvent(event commondomain.Event) (eventPayload interface{}) {
 			PlaylistID: uuid.UUID(currEvent.PlaylistID),
 			OwnerID:    uuid.UUID(currEvent.OwnerID),
 		}
+	case domain.PlaylistNameChanged:
+		eventPayload = struct {
+			PlaylistID uuid.UUID `json:"playlist_id"`
+			Name       string    `json:"name"`
+		}{
+			PlaylistID: uuid.UUID(currEvent.PlaylistID),
+			Name:       currEvent.NewName,
+		}
+	case domain.PlaylistItemAdded:
+		eventPayload = struct {
+			PlaylistID     uuid.UUID `json:"playlist_id"`
+			PlaylistItemID uuid.UUID `json:"playlist_item_id"`
+			ContentID      uuid.UUID `json:"content_id"`
+		}{
+			PlaylistID:     uuid.UUID(currEvent.PlaylistID),
+			PlaylistItemID: uuid.UUID(currEvent.PlaylistItemID),
+			ContentID:      uuid.UUID(currEvent.ContentID),
+		}
+	case domain.PlaylistItemRemoved:
+		eventPayload = struct {
+			PlaylistID     uuid.UUID `json:"playlist_id"`
+			PlaylistItemID uuid.UUID `json:"playlist_item_id"`
+		}{
+			PlaylistID:     uuid.UUID(currEvent.PlaylistID),
+			PlaylistItemID: uuid.UUID(currEvent.PlaylistItemID),
+		}
+	case domain.PlaylistRemoved:
+		eventPayload = struct {
+			PlaylistID uuid.UUID `json:"playlist_id"`
+			OwnerID    uuid.UUID `json:"owner_id"`
+		}{
+			PlaylistID: uuid.UUID(currEvent.PlaylistID),
+			OwnerID:    uuid.UUID(currEvent.OwnerID),
+		}
 	}
 	return
 }
