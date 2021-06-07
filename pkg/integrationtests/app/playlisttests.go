7194e64c5311c6b05ca14c9129d8598939770c81
UsingCoding <extendedmoment@outlook.com>
2021-06-07 23:09:05 +0300

    Добавил тесты на создание плейлистов, убрал лишнюю миграцию

--- pkg/integrationtests/app/playlisttests.go
+++ pkg/integrationtests/app/playlisttests.go
@@ -1,9 +1,50 @@
 package app
 
+import (
+	"github.com/CuriosityMusicStreaming/ComponentsPool/pkg/app/auth"
+	"github.com/google/uuid"
+)
+
 func playlistTests(serviceApiFacade *playlistServiceApiFacade, container UserContainer) {
 	createPlaylist(serviceApiFacade)
 }
 
 func createPlaylist(serviceApiFacade *playlistServiceApiFacade) {
+	user := auth.UserDescriptor{UserID: uuid.New()}
 
+	firstPlaylistName := "Gibberish 1000 hours"
+	secondPlaylistName := "Chill out"
+
+	{
+		firstPlaylistID, err := serviceApiFacade.CreatePlaylist(firstPlaylistName, user)
+		assertNoErr(err)
+
+		playlists, err := serviceApiFacade.GetUserPlaylists(user)
+		assertNoErr(err)
+
+		assertEqual(1, len(playlists.Playlists))
+
+		playlist := playlists.Playlists[0]
+
+		assertEqual(firstPlaylistID, playlist.PlaylistID)
+		assertEqual(firstPlaylistName, playlist.Name)
+		assertEqual(user.UserID.String(), playlist.OwnerID)
+
+		secondPlaylistID, err := serviceApiFacade.CreatePlaylist(secondPlaylistName, user)
+		assertNoErr(err)
+
+		playlists, err = serviceApiFacade.GetUserPlaylists(user)
+		assertNoErr(err)
+
+		assertEqual(2, len(playlists.Playlists))
+
+		secondPlaylist, err := serviceApiFacade.GetPlaylist(secondPlaylistID, user)
+		assertNoErr(err)
+
+		assertEqual(secondPlaylistName, secondPlaylist.Name)
+		assertEqual(user.UserID.String(), secondPlaylist.OwnerID)
+
+		assertNoErr(serviceApiFacade.DeletePlaylist(firstPlaylistID, user))
+		assertNoErr(serviceApiFacade.DeletePlaylist(secondPlaylistID, user))
+	}
 }
--- pkg/integrationtests/app/tests.go
+++ pkg/integrationtests/app/tests.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"context"
 	"github.com/CuriosityMusicStreaming/ComponentsPool/pkg/app/auth"
 	playlistserviceapi "playlistservice/api/playlistservice"
 )
@@ -24,4 +25,49 @@ func RunTests(playlistServiceClient playlistserviceapi.PlayListServiceClient, co
 type playlistServiceApiFacade struct {
 	client     playlistserviceapi.PlayListServiceClient
 	serializer auth.UserDescriptorSerializer
+}
+
+func (facade *playlistServiceApiFacade) CreatePlaylist(title string, userDescriptor auth.UserDescriptor) (string, error) {
+	userToken, err := facade.serializer.Serialize(userDescriptor)
+	assertNoErr(err)
+
+	resp, err := facade.client.CreatePlaylist(context.Background(), &playlistserviceapi.CreatePlaylistRequest{
+		Name:      title,
+		UserToken: userToken,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.PlaylistID, nil
+}
+
+func (facade *playlistServiceApiFacade) GetPlaylist(playlistID string, userDescriptor auth.UserDescriptor) (*playlistserviceapi.GetPlaylistResponse, error) {
+	userToken, err := facade.serializer.Serialize(userDescriptor)
+	assertNoErr(err)
+
+	return facade.client.GetPlaylist(context.Background(), &playlistserviceapi.GetPlaylistRequest{
+		PlaylistID: playlistID,
+		UserToken:  userToken,
+	})
+}
+
+func (facade *playlistServiceApiFacade) GetUserPlaylists(userDescriptor auth.UserDescriptor) (*playlistserviceapi.GetUserPlaylistsResponse, error) {
+	userToken, err := facade.serializer.Serialize(userDescriptor)
+	assertNoErr(err)
+
+	return facade.client.GetUserPlaylists(context.Background(), &playlistserviceapi.GetUserPlaylistsRequest{
+		UserToken: userToken,
+	})
+}
+
+func (facade *playlistServiceApiFacade) DeletePlaylist(playlistID string, userDescriptor auth.UserDescriptor) error {
+	userToken, err := facade.serializer.Serialize(userDescriptor)
+	assertNoErr(err)
+
+	_, err = facade.client.RemovePlaylist(context.Background(), &playlistserviceapi.RemovePlaylistRequest{
+		PlaylistID: playlistID,
+		UserToken:  userToken,
+	})
+	return err
 }
